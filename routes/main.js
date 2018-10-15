47c34d34899f5d329a8d256434c0fdf0772cf531
Denni Bevilacqua <dennib@live.it>
2018-10-15 21:53:26 +0200

    Correctly initialize and mantain TCP connection, passing dummy data through "/communicate" route

--- routes/main.js
+++ routes/main.js
@@ -1,42 +1,19 @@
-const net = require('net')
-
 // MAIN ROUTES FILE
-const routes = function(app) {
+const routes = function(app, socket) {
 
     // Default example route
     app.get('/', (req, res) => {
-
         res.send('Hello World!');
     });
+    
     // Dummy TCP communication
     app.get('/communicate', (req, res) => {
-
-    	// Create TCP Connection client
-        const client = net.createConnection(
-        	{ 	
-        		port: 9000,
-        		host: 'localhost'
-        	},
-        	() => {
-	            // 'connect' listener
-	            console.log('connected to server!')
-	            client.write('Hi from NodeJs!\r\n')
-       		})
-
-        // Listen for incoming data and log it to console
-        client.on('data',
-    		(data) => {
-	            console.log(data.toString())
-	            //client.end()
-    		})
-
-        // Notifiy connection closing to console
-        client.on('end',
-        	() => {
-            	console.log('disconnected from server')
-        	})
-
-        res.send('OK: Message sent');
+        // Write data through the socket
+        socket.write('Hi from /communitcate route!\r\n',
+            // Send http response when finished writing to socket
+            () => {
+                res.send('OK: Message sent')
+            })
     })
 }
 // Exporting module
