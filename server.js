0dcd36c7389ca4ebc0c7867dc03d9702e9e3ac56
Denni Bevilacqua <dennib@live.it>
2018-10-19 21:26:15 +0200

    HBS view engine configuration, "views" and "layouts" folders, "main.hbs" layout file and "index.hbs" view

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
+var path = require('path');
+const exphbs = require('express-handlebars')
 const routes = require('./routes/main')
 const app = express()
 
 // Declaring "public" folder as static folder
 app.use(express.static('./public'))
+
+// Registering Handlebars as default view engine
+app.engine('hbs', exphbs({
+	// Handlebars configuration
+	extname: '.hbs',
+	layoutsDir: path.join(__dirname,'./views/layouts'),
+	defaultLayout: 'main'
+}))
+app.set('view engine', 'hbs')
+
+// Registering "views" folder
+app.set('views', path.join(__dirname,'./views'));  
 
 // Creating TCP Socket connection
 const socket = require('./socket/socket')
