bc3200310f0fa65f3a1a0bd36aea86ac876c3785
Denni Bevilacqua <dennib@live.it>
2018-10-31 22:04:08 +0100

    Preparing to split WebUI into its own repository

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// Gulp Configuration file
+
+'use strict';
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync')
+var nodemon = require('nodemon')
+ 
+sass.compiler = require('node-sass');
+ 
+// Sass compile task
+gulp.task('sass', function () {
+  return gulp.src('./webapp/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+    .pipe(gulp.dest('./webapp/public/css'));
+});
+
+// Sass compile task :watch
+gulp.task('sass:watch', function () {
+  gulp.watch('./webapp/sass/**/*.scss', ['sass']);
+});
+
+// BrowserSync task
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:3000",
+		files: ['./**/*.*'],
+        port: 5000,
+	});
+});
+
+// Nodemon task
+gulp.task('nodemon', function (cb) {
+	var started = false
+	return nodemon({
+		script: './webapp/server.js'
+	}).on('start', function () {
+		// to avoid nodemon being started multiple times
+		// thanks @matthisk
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
+
+// Default task: start BrowserSync, Nodemon and compile sass
+gulp.task('default', ['browser-sync','sass:watch'], function () {
+});
